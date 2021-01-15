b7dbb82acdc3d6c3e58e574ecb0c9b4c4043e0ae
Sandro <SGamrekelashvili@gmail.com>
2021-01-15 15:45:50 +0400

    My

---
+++ L5/L5/react-app/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
+import List from './components/List';
+
+ReactDOM.render(
+<List />,
+document.getElementById('root')
+);
+
+reportWebVitals();
